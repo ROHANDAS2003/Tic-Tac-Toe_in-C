4ecb67a193309da956dcb73e05757ac6f06bebd8
Rohan das <110011068+ROHANDAS2003@users.noreply.github.com>
2022-12-23 16:18:35 +0530

    Update tic-tac-toe.c
    Added a function to print the board with 9 empty places.

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 #include<conio.h>
 
+char board[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+void printBoard()
+{
+    system("CLS");
+    printf("\n\n");
+    printf("====: TIC TAC TOE :====\n\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c  \n", board[1], board[2], board[3]);
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c  \n", board[4], board[5], board[6]);
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c  \n", board[7], board[8], board[9]);
+    printf("     |     |     \n");
+    printf("\n\n");
+}
+
 void main()
 {
+    printBoard();
 }
